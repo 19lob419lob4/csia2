1e65ee65dff3ddfd61a4261b92b54f5bd7ea3379
malcomzee09 <69036772+malcomzee09@users.noreply.github.com>
2020-12-06 17:24:01 +0800

    working on flashcards

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -17,14 +17,22 @@ class Menu extends React.Component {
 
         subjectFocus:false,
         subjectData: null,
+        activeSubject: 0,
         activeTopic: 0,
 
         keywords: ['is','are','because'],
 
         editMode: false,
         editValues: [], //for editing input in react...
+        editingObj: -1,
+        editPos: [0,0],  
         newEdit: false,
-        newEditValue: ''
+        newEditValue: '',
+
+        flashcardMode: false,
+        flashcardDeck:[],
+        activeDeck: 0,
+        openDeck: false
 
     }
     // this.addSubject.bind = this.addSubject.bind(this);
@@ -32,13 +40,16 @@ class Menu extends React.Component {
     this.handleChange = this.handleChange.bind(this);
     this.changeEditValue = this.changeEditValue.bind(this);
     this.addEditObj = this.addEditObj.bind(this);
+    this.loadFlashcardDeck = this.loadFlashcardDeck.bind(this);
 
   }
 
   handleChange(x,event){
     let currentValues = this.state.editValues;
     currentValues[x] = event.target.value;
-    this.setState({editValues:currentValues})
+
+    this.setState({editValues:currentValues, editingObj:x, editPos:[event.target.selectionStart,event.target.selectionEnd]})
+
   }
 
   changeEditValue(event){
@@ -47,7 +58,21 @@ class Menu extends React.Component {
 
   async componentDidMount(){
     this.getData()
+
+
+    window.addEventListener("keyup",()=>{
+      let editobj = document.getElementById('e' +this.state.editingObj);
+
+      if(editobj!=null){
+        editobj.focus()
+        editobj.selectionStart=this.state.editPos[0];
+        editobj.selectionEnd=this.state.editPos[1];
+      }
+
+    })
+    
   }
+
 
   async getData(){
     axios.get('http://localhost:3001/subjects')
@@ -65,6 +90,19 @@ class Menu extends React.Component {
   switchTopic(x){
     this.setState({activeTopic:x})
 
+    let values = [];
+    let statementList = this.state.subjects[this.state.activeSubject].topics[x].content;
+    for(let i=0; i<statementList.length; i++){
+      let before = statementList[i].before;
+      let keyword = statementList[i].keyword ==-1?'':this.state.keywords[statementList[i].keyword];
+      let after = this.renderARE(statementList[i].after);
+
+
+
+      values.push(before + ' ' + keyword + ' ' + after);        
+    }
+
+    this.setState({editValues:values});
 
   }
 
@@ -88,7 +126,15 @@ class Menu extends React.Component {
   
   loadSubject(x){
 
-    this.setState({subjectFocus:true, subjectData:this.state.subjects[x]})
+  
+
+    this.setState({activeSubject:x, subjectData:this.state.subjects[x]})
+
+    if(this.props.mode==0){
+      this.setState({subjectFocus:true, flashcardMode:false})
+    }else{
+      this.setState({flashcardMode:true, subjectFocus:false})
+    }
 
     //setup editable content...
     let values = [];
@@ -107,16 +153,18 @@ class Menu extends React.Component {
   }
 
   addEditObj(){
-    this.setState({newEdit:true})
+    if(this.state.newEditValue!=''){
+      this.setState({newEdit:true})
     
-    let currentValues = this.state.editValues;
-    currentValues.push(this.state.newEditValue);
-
-    this.setState({editValues:currentValues, newEditValue:'', newEdit:false})
-
-    var addEditObj = document.getElementById('addEditObj');
-    addEditObj.value = ''
-    addEditObj.focus();
+      let currentValues = this.state.editValues;
+      currentValues.push(this.state.newEditValue);
+  
+      this.setState({editValues:currentValues, newEditValue:'', newEdit:false})
+  
+      var addEditObj = document.getElementById('addEditObj');
+      addEditObj.value = ''
+      addEditObj.focus();
+    }
 
   }
 
@@ -137,7 +185,7 @@ class Menu extends React.Component {
 
           let before = this.state.editValues[j].substring(0,keywordIndex);
 
-          let after = this.state.editValues[j].substring(keywordIndex+keyword.length);
+          let after = this.state.editValues[j].substring(keywordIndex+keyword.length+2);
 
           let afterARR = this.saveARE(after);
           //update keywordStatements
@@ -151,17 +199,17 @@ class Menu extends React.Component {
       if(keywordStatement!=null){
         updatedStatements.push(keywordStatement)
       }else{
-        updatedStatements.push({before:this.state.editValues[j], keyword:-1, after:[]})
+        if(!(!this.state.editValues[j].replace(/\s/g, '').length)){
+          updatedStatements.push({before:this.state.editValues[j], keyword:-1, after:[]})
+        }
       }
     }
     //update database...
     let currentData = this.state.subjectData;
     
-    
-
     currentData.topics[this.state.activeTopic].content = updatedStatements;
     
-    console.log(currentData)
+    //console.log(currentData)
 
     let putAddress = 'http://localhost:3001/subjects/' + this.state.subjectData._id;
     axios.put(putAddress,currentData)
@@ -174,17 +222,23 @@ class Menu extends React.Component {
     
 
     //return back to menu
-    this.setState({editMode:false})
+    this.setState({editMode:false, editValues:[]})
+    this.forceUpdate()
+    this.switchTopic(this.state.activeTopic)
+  }
 
-     
-    // this.setState({subjectData:currentData})
- 
+
+  loadFlashcardDeck(x){
+    this.forceUpdate()
+    let deckData = this.state.subjectData.topics[x].content
+    this.setState({flashcardDeck:deckData, openDeck:true})
+
   }
 
   render(){
 
     let renderSubjects;
-    if (this.state.loadingData==false){
+    if (this.state.loadingData==false && this.state.flashcardMode==false){
       renderSubjects = this.state.subjects.map((subject,x) =>
         <a 
         className='subjectItem' 
@@ -239,8 +293,8 @@ class Menu extends React.Component {
     //gen array of changeable 'values' in state...
     if(this.state.editMode){
       editContent = this.state.editValues.map((editvalue,x)=>
-        <div className='editObj' key={uniqid()}>
-          <textarea id={x} type="text" value={editvalue} onChange={(e)=>this.handleChange(x,e)}/> 
+        <div className='editObj' key={uniqid()} >
+          <textarea id={'e'+x} key={uniqid()} type="text" value={editvalue} onChange={(e)=>this.handleChange(x,e)}/> 
 
         </div>
 
@@ -251,6 +305,32 @@ class Menu extends React.Component {
 
 
 
+    let flashcardTopicMenu;
+
+    if(this.state.flashcardMode){
+      flashcardTopicMenu = this.state.subjectData.topics.map((topic, x)=>
+        <a className='flashCardTopicItem' key={uniqid()} onClick={()=>this.loadFlashcardDeck(x)}>
+          <p style={{color:'white'}}>{topic.topicName}</p>
+          <p style={{color:'red'}}>{topic.content.length} cards</p>
+        </a>
+      )
+    }
+
+
+
+
+    let flashcardDeck;
+
+    if(this.state.openDeck){
+      flashcardDeck = this.state.flashcardDeck.map((card, x)=>
+        <div key={uniqid()} className='flashcard' style={{zIndex:x, position:'absolute',top:0, left:0}}>
+          <p>{card.before}</p>
+        </div>
+      )
+    }
+
+
+
 // {/* mode this.props ==mode */}
     return(
 
@@ -258,7 +338,7 @@ class Menu extends React.Component {
 
         {this.state.subjectFocus==false?(
 
-          <div className='subjectWrapper'>
+          <div className='subjectWrapper'  style={this.state.flashcardMode?{display:'none'}:{}}>
                       
           {this.state.loadingData || !this.state.subjects ?(<div></div>):(
 
@@ -272,7 +352,7 @@ class Menu extends React.Component {
 
             {this.state.editMode==false?(
             <div className='focusMenu' style={this.props.mode==0?{backgroundColor: 'royalblue'}:{backgroundColor: 'red'}}>
-              <button className ='backButton' onClick={()=>this.setState({subjectFocus:false})}>Back</button>
+              <button className ='backButton' onClick={()=>this.setState({subjectFocus:false, activeTopic:0})}>Back</button>
               {focusMenu}
             </div>
 
@@ -327,6 +407,42 @@ class Menu extends React.Component {
 
         )}
 
+        {/* flashcard section.... */}
+
+        {this.state.flashcardMode==true?(
+
+          <div className='flashCardWrapper'>
+                      
+            {this.state.loadingData ?(<div></div>):(
+              
+              <div className='flashCardItemWrapper'>
+                <div className="exitFlashcards">
+                  <button onClick={()=>this.setState({flashcardMode:false})}>Back</button>
+                </div>
+
+                
+                {this.state.openDeck==false?flashcardTopicMenu:(
+                <div style={{position:'relative', gridColumn:'1/10'}}>
+                  {flashcardDeck}
+                  <div className="flashCardButtons">
+                    <button>Again</button>
+                    <button>OK</button>
+                    <button>Good</button>
+
+                  </div>
+                </div>)}
+                
+                
+
+              </div>  
+
+
+
+            )}
+
+          </div>):<div></div>}
+
+          
 
       </div>
       
